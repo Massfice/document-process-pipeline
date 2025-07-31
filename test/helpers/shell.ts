2b070e343f6c653b7fd223b38e3a2bf002f6ec25
massfice <adrian.marian.tomasz.larysz@gmail.com>
2025-07-31 12:52:03 +0200

    test setup - shell and vitest config

---
+++ test/helpers/shell.ts
@@ -0,0 +1,62 @@
+import { vi } from 'vitest';
+import { cli } from '../../src/cli';
+
+type ShellFunc = (
+    args: string[],
+) => Promise<{ logs: string[] }>;
+
+export class ShellError extends Error {
+    constructor(
+        message: string,
+        public readonly logs: string[],
+    ) {
+        super(message);
+    }
+}
+
+export const shell: ShellFunc = async (args) => {
+    const logs: string[] = [];
+
+    const logFunction = (message: string) => {
+        if (message === undefined || message === null) {
+            return;
+        }
+        logs.unshift(message);
+    };
+
+    const exitSpy = vi
+        .spyOn(process, 'exit')
+        .mockImplementation((code) => {
+            throw new ShellError(
+                `Process exited with code: ${code}`,
+                logs,
+            );
+        });
+
+    const logSpy = vi
+        .spyOn(console, 'log')
+        .mockImplementation(logFunction);
+    const errorSpy = vi
+        .spyOn(console, 'error')
+        .mockImplementation(logFunction);
+
+    try {
+        await cli(args);
+
+        exitSpy.mockRestore();
+        logSpy.mockRestore();
+        errorSpy.mockRestore();
+
+        return { logs };
+    } catch (error) {
+        exitSpy.mockRestore();
+        logSpy.mockRestore();
+        errorSpy.mockRestore();
+
+        if (error instanceof ShellError) {
+            throw error;
+        }
+
+        throw new ShellError(String(error), logs);
+    }
+};
